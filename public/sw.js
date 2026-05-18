b596c2c62bd9e67ac1e7e272a126867db8d06220
Anirudha Dey <deyr3307@gmail.com>
2026-05-18 14:02:30 +0600

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,23 @@
-self.addEventListener('install', (e) => console.log('SW installed'));
-self.addEventListener('fetch', (e) => {});
+const CACHE_NAME = 'covergen-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
